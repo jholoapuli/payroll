b458193e95a5088d63555acc78993e2df1f9a759
apuli <jholoapuli00@gmail.com>
2021-02-16 13:35:05 -0800

    merge ui branch

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+Use App\Models\Department;
+use DB;
 class DepartmentController extends Controller
 {
-    //
+    public function __construct(Department $department, Request $request)
+    {
+        $this->department = $department;
+        $this->request = $request;
+    }
+
+    public function index(){  
+
+            
+        $department = $this->department;
+        return dd($department);
+    }
+
+    public function create_department()
+    {
+
+        $department = $this->department->create(
+
+            $this->request->except('_token')
+            
+         );
+         return Redirect::route('crud');
+    }
+
+    public function update_form($id){
+        return view('form.update')->with([
+            'data' => $this->personnel->find($id)
+        
+            ]);
+    }
 }
